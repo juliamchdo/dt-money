582b70d3d9f960be30039c94bcdd0160907088eb
juliamchdo <juliamachado226@gmail.com>
2025-02-01 12:49:22 -0300

    initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+*{
+    margin:  0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    background-color: ${(props) => props.theme["green-500"]};
+    color: ${(props) => props.theme["gray-100"]};
+    -webkit-font-smoothing: antialiased;
+}
+
+body, input, textarea {
+    font: 400 1rem Roboto, sans-serif;
+}
+
+:focus{
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
+}
+`;
