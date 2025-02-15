b817d0255098be5849271dc7670afa236ea87367
juliamchdo <juliamachado226@gmail.com>
2025-02-15 15:42:37 -0300

    add summary and table api connections

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useEffect, useState } from "react";
+
+export interface Transaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+interface TransactionsContenxtType {
+  transactions: Transaction[];
+}
+
+interface TransactionProviderProps {
+  children: React.ReactNode;
+}
+
+export const TransactionsContext = createContext(
+  {} as TransactionsContenxtType
+);
+
+export function TransactionsProvider({ children }: TransactionProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3000/transactions");
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
